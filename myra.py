b044f5852768d1c425e0c406c9a3f9a23c14ccb1
jrbalderrama <jrbalderrama@gmail.com>
2013-08-29 16:37:02 +0200

    adding __future__ support for compatibility

--- myra.py
+++ myra.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python
 
+from __future__ import print_function
+from __future__ import unicode_literals
+
 import errno
 import os
 import tempfile
 import subprocess
 import sys
 from lockfile import FileLock
+
 
 STATIONS_FILE_NAME = ".myrarc"
 # .mirarc file format located at $HOME dir
@@ -14,34 +18,31 @@ STATIONS_FILE_NAME = ".myrarc"
 # cz1 http://netshow.play.cz:8000/radio1.mp3 Radio 1 CZ
 
 
-class Radio(object):
-
-
-    def __init__(self, identifier, url, description=None):
-        self.identifier = identifier
-        self.url = url
-        self.description = description
-
-    def play(self):
-        lockfile = FileLock(tempfile.gettempdir() + os.sep + STATIONS_FILE_NAME)
-        if lockfile.is_locked():
-            sys.stderr.write('\tmyra radio player is already on!\n')
-            sys.exit(errno.EALREADY)
-        with lockfile:
+def play(identifier, url, description=None):
+    file = os.path.join(tempfile.gettempdir(), STATIONS_FILE_NAME)
+    lockfile = FileLock(file)
+    if lockfile.is_locked():
+        sys.stderr.write('\tmyra radio player is already on!\n')
+        sys.exit(errno.EALREADY)
+    with lockfile:
+        try:
             message = '\tNow playing ' + \
-                      (self.identifier if not self.description else self.description) + '!'
-            command = ['mplayer', '-really-quiet', '-cache', '256', self.url]
-            if self.url.endswith(('m3u', 'pls', 'asx')) or \
-               self.url.startswith('mms'):
+                      (identifier if not description else description) + '!'
+            command = ['mplayer', '-really-quiet', '-cache', '256', url]
+            if url.endswith(('m3u', 'pls', 'asx')) or url.startswith('mms'):
                 command.insert(-1, '-playlist')
-            elif self.url.startswith('rtmp'):
+            elif url.startswith('rtmp'):
                 print('work in progress')
+                sys.exit(errno.ENOENT)
                 # TODO support rtmp streams using pipes
                 # between rtmpdump and mplayer
                 # vlc -Idummy URL
                 # cmd=['rtmpdump', '-v', '-r', self.url, '|', 'mplayer', '-']
             print(message)
             execute(command, True)
+        ## detect stale lock files if keyboard interrupt
+        except KeyboardInterrupt:
+            lockfile.release()
 
 
 def execute(command, devnull=False):
@@ -52,7 +53,7 @@ def execute(command, devnull=False):
             with open(os.devnull, 'w') as filenull:
                 subprocess.call(command, stdout=filenull, stderr=filenull)
     except KeyboardInterrupt:
-        # todo add pythonic reset
+        # TODO add pythonic reset
         command = ['reset']
         subprocess.call(command)
         sys.exit(errno.EINTR)
@@ -71,7 +72,7 @@ def main(argv):
     path = os.path.join(os.getenv('HOME'), STATIONS_FILE_NAME)
     lines = read_file(path)
     # radios = map(lambda x: x.split(None, 2), descriptions)
-    radios = [line.split(None, 2) for line in lines]
+    radios = [line.decode('utf-8').split(None, 2) for line in lines]
     if not argv:
         print('Usage: myra [ID|URL]\n')
         print('\tID\tdescription\n')
@@ -80,20 +81,17 @@ def main(argv):
         sys.exit(errno.EAGAIN)
     parameter = argv[0]
     try:
-        identifier = None
-        url = None
-        description = None
         if '://' not in parameter:
-            dictionary = dict([(column[0], (column[1], ''.join(column[2:]).rstrip()))
-                               for column in radios])
+            dictionary = dict([
+                (column[0], (column[1], ''.join(column[2:]).rstrip()))
+                for column in radios])
             identifier = parameter
             url = dictionary[parameter][0]
             description = dictionary[parameter][1]
         else:
             identifier = "radio from URL"
             url = parameter
-        radio = Radio(identifier, url, description)
-        radio.play()
+        play(identifier, url, description)
     except KeyError:
         sys.stderr.write('\tCannot play ' + parameter + '\n')
 
